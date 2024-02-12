714c13a31618edb27c58e57aba9e6267f03e564a
CBassSebastian <sjcalixtro@gmail.com>
2024-02-12 16:42:24 -0500

    Added flight data file and made function to read it.

--- Read-Write Data.py
+++ Read-Write Data.py
@@ -1,3 +1,9 @@
-print("Hello World!")
-print("This is a test.")
-print("Last test, I promise :)")
+import csv
+from time import sleep
+
+with open ('Airline_Delay_Cause.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    for line in csv_reader:
+        print(line)
+        sleep(1)
