576644e1132f9855d161e7ff87fb5f0e44a95023
CBassSebastian <sjcalixtro@gmail.com>
2024-02-29 14:21:28 -0500

    Edited controller file.

--- astController.py
+++ astController.py
@@ -1,4 +1,12 @@
 import read_write_data
 
-print("AST Controller")
+print("AST Controller/n")
 
+user_input = input("\nEnter (d) for Development Mode or (u) User Mode.\n")
+
+if(user_input.lower() == "d"):
+    print("In Development Mode")
+elif(user_input.lower() == "u"):
+    print("In User Mode")
+else:
+    print("Unknown Mode")
