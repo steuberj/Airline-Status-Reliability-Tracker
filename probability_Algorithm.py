4db960ec316160ca53a03a5b77ef78d141c7491c
CBassSebastian <sjcalixtro@gmail.com>
2024-03-23 18:12:54 -0400

    Fixed a lot of broken stuff and have normalized data, but no accurate predictions.

--- probability_Algorithm.py
+++ probability_Algorithm.py
@@ -4,6 +4,8 @@ from keras import layers
 import numpy as np
 import pandas as pd
 
+'''
+
 np.set_printoptions(precision=3, suppress=True)
 
 modelData = pd.read_csv("Airline_Delay_Cause_No_Extra_Info.csv")
@@ -50,3 +52,22 @@ flightDelay_preprocessing = tf.keras.Model(inputData, preprocessed_inputs_cat)
 modelDataCopy_dict = {name: np.array(value) for name, value in modelDataCopy.items()}
 #print(modelDataCopy_dict)
 copy_dict = {name:values[:1] for name, values in modelDataCopy_dict.items()}
+flightDelay_preprocessing(copy_dict)
+
+'''
+
+historical_data = pd.read_csv('Airline_Delay_Cause_No_Extra_Info.csv')
+arr_labels = historical_data.pop('weather_delay')
+data_arr = np.array(historical_data)
+arr_labels = np.array(arr_labels)
+print(data_arr)
+normalized_arr = (data_arr - np.min(data_arr)) / (np.max(data_arr) - np.min(data_arr))
+print(normalized_arr)
+normalized_arr_labels = (arr_labels - np.min(arr_labels)) / (np.max(arr_labels) - np.min(arr_labels))
+
+
+flightDelay_model = tf.keras.Sequential([layers.Dense(16), layers.Dense(60), layers.Dense(60), layers.Dense(1)])
+
+flightDelay_model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), optimizer=tf.keras.optimizers.Adam())
+
+flightDelay_model.fit(x=normalized_arr, y=normalized_arr_labels, epochs=20)
--- read_write_data.py
+++ read_write_data.py
@@ -6,14 +6,21 @@ def modifyCSV():
         csv_reader = csv.DictReader(csv_file)
 
         with open ('Airline_Delay_Cause_No_Extra_Info.csv', 'w', newline = '') as file:
-            reqCol = ["carrier", "airport", "arr_del15", "carrier_ct", "security_delay", "arr_flights", "carrier_delay", "arr_diverted",
-                  "weather_ct", "late_aircraft", "month", "year", "security_ct", "nas_ct", "arr_cancelled", "late_aircraft_ct",
+            reqCol = ["arr_del15", "carrier_ct", "security_delay", "arr_flights", "carrier_delay", "arr_diverted",
+                  "weather_ct", "security_ct", "nas_ct", "arr_cancelled", "late_aircraft_ct",
                   "arr_delay", "weather_delay", "nas_delay", "late_aircraft_delay"]
             csv_writer = csv.DictWriter(file, fieldnames = reqCol)
             csv_writer.writeheader()
             for line in csv_reader:
                 #del line['']
+                del line['month']
+                del line['year']
                 del line['carrier_name']
                 del line['airport_name']
+                del line['carrier']
+                del line['airport']
 
                 csv_writer.writerow(line)
+
+modifyCSV()
+print("Complete")
