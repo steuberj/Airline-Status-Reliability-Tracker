ce1f85dc68ef810bf6b814b27d92ed7e291a93b3
CBassSebastian <sjcalixtro@gmail.com>
2024-04-23 14:52:23 -0400

    Small change.

--- flightDataAPI.py
+++ flightDataAPI.py
@@ -1,14 +1,10 @@
 import aeroapi_python 
 
-def flightdata():
-    flightinfo = []
-
+def flightdata(airport, airline):
     api_key = 'lpMGPqwMlgA4guMGgD96g4YOc0CznoGG'
     aeroapi = aeroapi_python.AeroAPI(api_key)
     airports = aeroapi.airports
-    airport_info = airports.scheduled_arrivals("MIA", "DAL")
-    #print(airport_info
-    #print(airport_info.keys())
+    airport_info = airports.scheduled_arrivals(airport, airline)
     flightinfo = []
     for item in airport_info['scheduled_arrivals']:
       flightinfo.append(search_dict(item, 'ident_iata'))
@@ -16,8 +12,6 @@ def flightdata():
       flightinfo.append(search_dict(item['destination'], 'code_iata'))
       flightinfo.append(search_dict(item, 'route_distance'))
       flightinfo.append(search_dict(item, 'arrival_delay'))
-      #print("")
-
     return flightinfo
 
 def search_dict(d, key):
@@ -25,5 +19,3 @@ def search_dict(d, key):
         return f"{d[key]}"
      else:
         return "Key not found."
-     
-print(flightdata())
